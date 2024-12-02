f0093b9f25edc7de3df4b82280a5e569aa2d01ee
AnyaKhemlani <askny2014@hotmail.com>
2024-12-02 16:24:45 -0500

    update home.py
    home.py now has buttons for each user type.

--- app/src/Home.py
+++ app/src/Home.py
@@ -34,7 +34,7 @@ SideBarLinks(show_home=True)
 
 # set the title of the page and provide a simple prompt. 
 logger.info("Loading the Home page of the app")
-st.title('CS 3200 Sample Semester Project App')
+st.title('Welcome to CareerCompass')
 st.write('\n\n')
 st.write('### HI! As which user would you like to log in?')
 
@@ -42,36 +42,49 @@ st.write('### HI! As which user would you like to log in?')
 # functionality, we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
-if st.button("Act as John, a Political Strategy Advisor", 
+if st.button("Act as Lily, a student applying to co-ops", 
             type = 'primary', 
             use_container_width=True):
     # when user clicks the button, they are now considered authenticated
     st.session_state['authenticated'] = True
     # we set the role of the current user
-    st.session_state['role'] = 'pol_strat_advisor'
+    st.session_state['role'] = 'srudent'
     # we add the first name of the user (so it can be displayed on 
     # subsequent pages). 
-    st.session_state['first_name'] = 'John'
+    st.session_state['first_name'] = 'Lily'
     # finally, we ask streamlit to switch to another page, in this case, the 
     # landing page for this particular user type
-    logger.info("Logging in as Political Strategy Advisor Persona")
-    st.switch_page('pages/00_Pol_Strat_Home.py')
+    logger.info("Logging in as student persona")
+    st.switch_page('pages/Student_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
+if st.button('Act as Anya, a company employee managing the co-op hiring process', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
+    st.session_state['role'] = 'company'
+    st.session_state['first_name'] = 'Anya'
+    st.switch_page('pages/Company_Home.py')
 
-if st.button('Act as System Administrator', 
+if st.button('Act as Kalina, a co-op advisor for students', 
+            type = 'primary', 
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'advisor'
+    st.session_state['first_name'] = 'Kaline'
+    st.switch_page('pages/Advisor_Home.py')
+
+if st.button('Act as Neel, an alumn of Northeastern', 
+            type = 'primary', 
+            use_container_width=True):
+    st.session_state['authenticated'] = True
+    st.session_state['role'] = 'alumn'
+    st.session_state['first_name'] = 'Neel'
+    st.switch_page('pages/Alumn_Home.py')
+
+if st.button('Act as Tarini, a system administrator of Career Compass', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'administrator'
     st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
-
-
-
+    st.switch_page('pages/Admin_Home.py')
