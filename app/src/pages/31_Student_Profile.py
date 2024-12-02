34c1dd62305097f7d47c6c9bcaa2bf533a8ebac5
lilybedi <bedichek.l@northeastern.edu>
2024-12-02 17:25:53 -0500

    Added Student profile, with Image, Stats and Resumes

---
+++ app/src/pages/31_Student_Profile.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+student_info, website_links = st.columns(2)
+
+# Custom CSS portion
+st.markdown(
+    """
+    <style>
+    .second {
+        background-color: #aaaaaa; 
+        display: inline-block;
+        margin: 0; 
+
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+with student_info:
+    image, stats = st.columns(2)
+    
+    with image:
+      st.markdown('<div class="second"> Image </div>', unsafe_allow_html=True)
+
+    with stats:
+      st.markdown('<div class="second"> Stats </div>', unsafe_allow_html=True)
+
+with website_links:
+    resumes = st.expander(label="Resumes")
+    #
+    # Data should come in [ResumeNames] as a list
+    # and [ResumeFiles] as a list, where item ResumeName[0]
+    # corresponds with [ResumeFile[0]]
+    #
+
+    # Delete when done - Sample Data
+    resume_names = ["Programming Resume", "Data Science Resume"]
+    resume_files = ["google.com", "yahoo.com"]
+    # Stop deleting
+
+    for i in range (0,len(resume_files)):
+      try:
+        url = resume_files[i]
+        name = resume_names[i]
+      except IndexError:
+         st.write("Resume link or name not provided")
+      resumes.markdown(f"[{name}]({url})") 
+
+    
+    url = "https://www.streamlit.io"
+
+
+
+
+
